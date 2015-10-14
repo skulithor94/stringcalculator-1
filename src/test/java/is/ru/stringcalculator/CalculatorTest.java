ac8dcef847abc0b21d240553cb360961b64b3b44
Skuli <skuliarnas@gmail.com>
2015-10-14 19:04:32 +0000

    Add for empty string works

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -13,20 +13,4 @@ public class CalculatorTest {
 	public void testEmptyString() {
 		assertEquals(0, Calculator.add(""));
 	}
-
-	@Test
-	public void testOneNumber() {
-		assertEquals(1, Calculator.add("1"));
-	}
-
-	@Test
-	public void testTwoNumbers() {
-		assertEquals(3, Calculator.add("1,2"));
-	}	
-
-	@Test
-    public void testMultipleNumbers(){
-    	assertEquals(6, Calculator.add("1,2,3"));
-    }
-
 }
